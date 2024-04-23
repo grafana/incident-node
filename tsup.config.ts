9835477ef1494aa721baff05fa3d457ebdaf349d
Christopher Lord <christopher@pliosoft.com>
2024-04-23 10:43:19 -0600

    fix: core-lib should work with eslint consistently; switch to pnpm (#5259)
    ### BACKGROUND: Why is this change required?
    
    core-lib has numerous small paper-cuts relating to exports, causing
    tools like eslint to not find definitions, or typescript compiler to not
    find certain definitions, or biome to not understand the types. I traced
    this down to problems relating to webpack not fully understanding module
    libraries, so this PR introduces some changes that help it along without
    losing the benefits of module lib.
    
    As part of this, I found it necessary to stop using lerna. It's becoming
    unreliable, and the dependent-build feature often does not work. This PR
    adopts `pnpm` across the board for FE development.
    
    That means commands have changed:
    
    * everything that was `lerna` or `yarn` is now `pnpm`:
      * `pnpm build`
      * `pnpm lint`
      * `pnpm format`
      * `pnpm install`
      * `pnpm install --save-dev @types/foo`
    
    ### VALIDATION: How does reviewer validate changes?
    
    There should be no user-visible changes here, purely dev-ex. Builds
    should go, tests should pass, and developers should see better eslint
    with respect to core-lib.
    
    ---------
    
    Co-authored-by: Jackson Coelho <fcjack@gmail.com>
    Co-authored-by: Thomas Wurmitzer <thomas.wurmitzer@grafana.com>

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entryPoints: ['src/index.ts'],
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  format: ['cjs', 'esm'],
+  bundle: true,
+  outDir: 'dist',
+});
